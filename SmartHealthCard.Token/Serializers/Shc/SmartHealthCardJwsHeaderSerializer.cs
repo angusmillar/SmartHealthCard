7d1d48ef9cafd727791bb270a26b957d53a6bcf4
angusmillar <angusbmillar@gmail.com>
2021-05-25 22:16:29 +1000

    Consoladated use of JSON serializers

--- SmartHealthCard.Token/Serializers/Shc/SmartHealthCardJwsHeaderSerializer.cs
+++ SmartHealthCard.Token/Serializers/Shc/SmartHealthCardJwsHeaderSerializer.cs
@@ -1,33 +1,24 @@
-﻿using Newtonsoft.Json;
-using SmartHealthCard.Token.Exceptions;
-using SmartHealthCard.Token.Model.Shc;
+﻿using SmartHealthCard.Token.Model.Shc;
+using SmartHealthCard.Token.Serializers.Json;
 using SmartHealthCard.Token.Serializers.Jws;
 using System;
-using System.IO;
-using System.Text;
-using static SmartHealthCard.Token.Encoders.Utf8EncodingSupport;
 
 namespace SmartHealthCard.Token.Serializers.Shc
 {
   public class SmartHealthCardJwsHeaderSerializer : IJwsHeaderSerializer
   {    
-    private readonly Newtonsoft.Json.JsonSerializer Serializer;
+    private readonly JsonSerializer Serializer;
     public SmartHealthCardJwsHeaderSerializer()
     {
-      this.Serializer = Newtonsoft.Json.JsonSerializer.CreateDefault();
+      Serializer = new Json.JsonSerializer(Minified: true);
     }
     
     public byte[] Serialize<T>(T Obj)
     {
       if (Obj is SmartHealthCareJWSHeaderModel SmartHealthCareJWSHeaderModel)
       {
-        Serializer.Formatting = Formatting.Indented;
-        var sb = new StringBuilder();
-        using var stringWriter = new StringWriter(sb);
-        using var jsonWriter = new JsonTextWriter(stringWriter);
-        Serializer.Serialize(jsonWriter, SmartHealthCareJWSHeaderModel);
-        string SmartHealthCardHeaderJson = sb.ToString();       
-        return GetBytes(SmartHealthCardHeaderJson);
+       
+        return Serializer.Serialize(SmartHealthCareJWSHeaderModel);
       }
       else
       {
@@ -36,14 +27,8 @@ namespace SmartHealthCard.Token.Serializers.Shc
     }
 
     public T Deserialize<T>(byte[] bytes)
-    {
-      string json = GetString(bytes);
-      using var stringReader = new StringReader(json);
-      using var jsonReader = new JsonTextReader(stringReader);
-      var SmartHealthCareJWSHeaderModel = Serializer.Deserialize<SmartHealthCareJWSHeaderModel>(jsonReader);
-      if (SmartHealthCareJWSHeaderModel == null)
-        throw new DeserializationException($"Unable to deserialize the JWS Header to type {typeof(SmartHealthCareJWSHeaderModel).Name}");
-      return (T)(object)SmartHealthCareJWSHeaderModel;      
+    {      
+      return (T)(object)Serializer.Deserialize<SmartHealthCareJWSHeaderModel>(bytes);      
     }
   }
 }
