998f030f2463f3101c013b8a48839609f1c9c0d4
angusmillar <angusbmillar@gmail.com>
2021-09-22 23:40:19 +1000

    Setting up for new test case to test invalid signatures

--- SmartHealthCard.Test/SmartHealthCardDecoderTest.cs
+++ SmartHealthCard.Test/SmartHealthCardDecoderTest.cs
@@ -73,6 +73,35 @@ namespace SmartHealthCard.Test
       Assert.NotNull(SmartHealthCardModel);
     }
 
+    [Fact]
+    public async void Decode_Token_Verify_InvalidTokenSignature_Certificate()
+    {
+      //### Prepare ######################################################
+      //Get the ECC certificate from the Cert and Private key PEM files
+      X509Certificate2 Certificate = CertificateSupport.GetCertificateFromPemFiles();
+
+      //The base of the URL where a validator will retrieve the public keys from (e.g : [Issuer]/.well-known/jwks.json) 
+      Uri Issuer = new Uri("https://sonichealthcare.com/something");
+      string SmartHealthCardJwsToken = await SmartHealthCardJwsSupport.GetJWSCovidExampleOneAsync(Certificate, Issuer);
+
+      //This testing JwksSupport class provides us with a mocked IJwksProvider that will inject the JWKS file
+      //rather than make the HTTP call to go get it from a public endpoint.
+      IJwksProvider MockedIJwksProvider = JwksSupport.GetMockedIJwksProvider(Certificate, Issuer);
+
+      //Instantiate the SmartHealthCard Decoder
+      SmartHealthCardDecoder Decoder = new SmartHealthCardDecoder(MockedIJwksProvider);
+
+      //### Act #######################################################
+
+      //Verify and Decode
+      SmartHealthCardModel SmartHealthCardModel = await Decoder.DecodeAsync(SmartHealthCardJwsToken, Verify: true);
+
+      //### Assert #######################################################
+
+      Assert.True(!string.IsNullOrWhiteSpace(SmartHealthCardJwsToken));
+      Assert.NotNull(SmartHealthCardModel);
+    }
+
 
   }
 }
---
+++ SmartHealthCard.Test/Support/SmartHealthCardIncorrectSignatureJwsEncoder.cs
@@ -0,0 +1,52 @@
+﻿using SmartHealthCard.Token.Algorithms;
+using SmartHealthCard.Token.Encoders;
+using SmartHealthCard.Token.JwsToken;
+using SmartHealthCard.Token.Serializers.Jws;
+using SmartHealthCard.Token.Support;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartHealthCard.Test.Support
+{
+  public class SmartHealthCardIncorrectSignatureJwsEncoder : IJwsEncoder
+  {
+    private readonly IAlgorithm Algorithm;
+    private readonly IJwsHeaderSerializer HeaderSerializer;
+    private readonly IJwsPayloadSerializer PayloadSerializer;
+    public SmartHealthCardIncorrectSignatureJwsEncoder(IJwsHeaderSerializer HeaderSerializer, IJwsPayloadSerializer PayloadSerializer, IAlgorithm Algorithm)
+    {
+      this.HeaderSerializer = HeaderSerializer;
+      this.PayloadSerializer = PayloadSerializer;
+      this.Algorithm = Algorithm;
+    }
+
+    public async Task<Result<string>> EncodeAsync<HeaderType, PayloadType>(HeaderType Header, PayloadType Payload)
+    {
+      Result<byte[]> HeaderSerializeResult = await HeaderSerializer.SerializeAsync(Header);
+      Result<byte[]> PayloadSerializeResult = await PayloadSerializer.SerializeAsync(Payload);
+      var CombineResult = Result.Combine(HeaderSerializeResult, PayloadSerializeResult);
+      if (CombineResult.Failure)
+        return Result<string>.Fail(CombineResult.ErrorMessage);
+
+      var HeaderSegment = Base64UrlEncoder.Encode(HeaderSerializeResult.Value);
+      var PayloadSegment = Base64UrlEncoder.Encode(PayloadSerializeResult.Value);
+
+      var BytesToSign = Utf8EncodingSupport.GetBytes($"{HeaderSegment}.{PayloadSegment}");
+
+      Result<byte[]> SignatureResult = Algorithm.Sign(BytesToSign);
+
+      Random Random = new Random();
+      Byte[] WickedBytes = new Byte[SignatureResult.Value.Length];
+      Random.NextBytes(WickedBytes);
+     
+      if (SignatureResult.Failure)
+        return Result<string>.Fail(SignatureResult.ErrorMessage);
+       
+      var SignatureSegment = Base64UrlEncoder.Encode(WickedBytes);
+
+      return Result<string>.Ok($"{HeaderSegment}.{PayloadSegment}.{ SignatureSegment}");
+    }
+  }
+
+  ///What to do, we need to create or overide this method in a new class called SmartHealthCardIncorrectSignatureJwsEncoder using only for testing.
+}
