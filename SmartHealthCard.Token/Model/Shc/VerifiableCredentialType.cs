8de9abfb1c4eb75ae474b10dc9e85c5c1759bebc
angusmillar <angusbmillar@gmail.com>
2021-10-07 11:20:41 +1000

    Added support for a new Verifiable Credential type of 'VerifiableCredential' in response to GitHub Issue: VerifiableCredentialTypes (vc.type) Was Not An Allowed Value #9 This particular type comes from the base W3's specification for Verifiable Credentials and was not highlighted as required in in the SMART Health Specification.  Reading that base spec here : https://www.w3.org/TR/vc-data-model/#types it is clear that all Verifiable Credentials tokens, SMART Health Card tokens included, should state that they are a Verifiable Credentials by setting the type of  'VerifiableCredential' in there Type property. Other down stream specifications for specialised types of Verifiable Credentials, such as SMART Health Cards, can then add their own new types for their purposes. The SMART Health Cards spec does this by adding the specialised types of (https://smarthealth.cards#health-card, https://smarthealth.cards#covid19, https://smarthealth.cards#immunization and https://smarthealth.cards#laboratory) as found here: https://spec.smarthealth.cards/vocabulary/

--- SmartHealthCard.Token/Model/Shc/VerifiableCredentialType.cs
+++ SmartHealthCard.Token/Model/Shc/VerifiableCredentialType.cs
@@ -1,14 +1,12 @@
 ﻿using SmartHealthCard.Token.Enums;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartHealthCard.Token.Model.Shc
 {
   public enum VerifiableCredentialType
-  {
+  {    
+    [EnumInfo(literal: "VerifiableCredential", description: "VerifiableCredential")]
+    VerifiableCredential,
     [EnumInfo(literal: "https://smarthealth.cards#health-card", description: "HealthCard")]
     HealthCard,
     [EnumInfo(literal: "https://smarthealth.cards#covid19", description: "Covid19")]
@@ -23,6 +21,7 @@ namespace SmartHealthCard.Token.Model.Shc
   {
     public static readonly Dictionary<string, VerifiableCredentialType> VerifiableCredentialTypeDictionary = new Dictionary<string, VerifiableCredentialType>()
         {
+          { VerifiableCredentialType.VerifiableCredential.GetLiteral(), VerifiableCredentialType.VerifiableCredential },
           { VerifiableCredentialType.Covid19.GetLiteral(), VerifiableCredentialType.Covid19 },
           { VerifiableCredentialType.HealthCard.GetLiteral(), VerifiableCredentialType.HealthCard },
           { VerifiableCredentialType.Immunization.GetLiteral(), VerifiableCredentialType.Immunization },
